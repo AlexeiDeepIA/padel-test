37baaed92d0c28131c20c05f2f8efa399fc9f5cf
AlexeiDeepIA <alexeipalacios@deepia.mx>
2024-02-13 17:00:44 -0600

    [ADD] New homescreen

--- src/app/darkmode/darkmode.component.ts
+++ src/app/darkmode/darkmode.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthGoogleService } from '../_services/auth-google.service';
 
 @Component({
   selector: 'app-darkmode',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./darkmode.component.scss']
 })
 export class DarkmodeComponent {
+
+  profileData: any;
+  picture: any;
+
+ constructor(private AuthGoogleService: AuthGoogleService){}  
+
+ ngOnInit() {
+  this.AuthGoogleService.initLogin().then(() => {
+    this.getProfileData();
+  });
+
+}
+
+setDefaultImage() {
+  this.profileData.picture =
+    'https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg';
+}
+
+  getProfileData() {
+    const metaData = this.AuthGoogleService.getProfile();
+    this.profileData = metaData;
+    this.picture = this.profileData.picture;
+  }
 
 }
